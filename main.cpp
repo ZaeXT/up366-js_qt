4cc6fedcd146870767112de4ad569c803e85f050
Akaze <83901773+ZaeXT@users.noreply.github.com>
2023-07-30 10:20:55 +0800

    Initlized Project.

--- main.cpp
+++ main.cpp
@@ -31,16 +31,19 @@ int main(int argc, char *argv[])
 
         // 在这里可以将输入内容保存到文件或数据库中
         // 这里只是简单地弹出一个消息框显示输入内容
-    QMessageBox::information(nullptr, "解析成功", "输入内容已保存!");
+QMessageBox::information(nullptr, "解析成功", "输入内容已解析!");
     aw = QString::fromStdString(js_regex(inputText.toStdString()));
     QMessageBox::information(nullptr, "Answer", aw);
     // qDebug() << js_regex(inputText.toStdString());
     qDebug() << aw; });
-
+    QPushButton quitb("Quit");
+    QObject::connect(&quitb, &QPushButton::clicked, [&]()
+                     { app.quit(); });
     // 创建一个布局，并将文本输入框和按钮添加到布局中
     QVBoxLayout layout;
     layout.addWidget(&textInput);
     layout.addWidget(&saveButton);
+    layout.addWidget(&quitb);
     // 创建一个窗口，并将布局设置为窗口的主布局
     QWidget window;
     window.setLayout(&layout);
